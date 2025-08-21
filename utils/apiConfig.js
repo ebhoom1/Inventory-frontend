dc7e0cb53842ec365d7cbcb9b56a484f692b5d0d
ebhoom1 <info.ebhoom@gmail.com>
2025-08-21 13:16:01 +0530

    updtaed deployed url

--- utils/apiConfig.js
+++ utils/apiConfig.js
@@ -1,3 +1,4 @@
 // export const API_URL ='http://13.203.39.180:5001';
 
- export const API_URL ='http://localhost:5001';
+ //export const API_URL ='http://localhost:5001';
+ export const API_URL ='http://13.203.39.180:5001';
