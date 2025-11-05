26f8fbb7a13be3ede828b52faa843466464a04d0
unknown <nihalaarpk@gmail.com>
2025-11-05 15:54:40 +0530

    changed api_url to deployed link

--- utils/apiConfig.js
+++ utils/apiConfig.js
@@ -1,9 +1,9 @@
 
- export const API_URL ='http://localhost:5001';
+//  export const API_URL ='http://localhost:5001';
  //export const API_URL ='http://13.203.39.180:5001';
 
 // ✅ production (deployed)
-// export const API_URL = 'https://api.safetik.in';
+export const API_URL = 'https://api.safetik.in';
 // export const API_URL = 'https://api.safetik.in';
 
 // Vite
